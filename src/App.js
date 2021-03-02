56340103164b513fa7cbbf3cb1dd4ca66a435eb4
Manami Ueda <manamiueda1@gmail.com>
2021-03-01 20:11:39 -0500

    rendering monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      dogs: [],
+    };
+  }
+
+  // componentDidMount() {
+  //   fetch("https://dog.ceo/api/breeds/image/random/3").then((response) =>
+  //     response.json().then((data) => this.setState({ dogs: data.message }))
+  //   );
+  // }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((array) => this.setState({ monsters: array }));
+  }
+
+  render() {
+    // console.log(this.state.dogs);
+
+    return (
+      <div className="App">
+        {/* <div>
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "hello friend" })}>
+            Change Text
+          </button>
+        </div> */}
+
+        {this.state.dogs.map((dog) => (
+          <img alt="randomDogs" src={dog} />
+        ))}
+
+        {this.state.monsters.map((monster) => (
+          <div>
+            <h1 key={monster.id}>{monster.name}</h1>
+            <h3>{monster.email}</h3>
+            <h3>{monster.username}</h3>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
